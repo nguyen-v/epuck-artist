0e086547c6e38a9683309d95d5b2f8ea29ddf768
Yassine Bakkali <ybakkali8@gmail.com>
2021-04-06 18:53:13 +0200

    Bug fixes in image_processing.c

--- src/modules/image_processing.c
+++ src/modules/image_processing.c
@@ -1,11 +1,11 @@
-#include "ch.h"
-#include "hal.h"
 #include <chprintf.h>
 #include <usbcfg.h>
 #include <math.h>
 
 #include <main.h>
 #include <camera/po8030.h>
+
+#include <image_processing.h>
 
 
 #define IM_LENGTH_PX 100
@@ -15,24 +15,24 @@
 #define RAD2DEG 180/M_PI
 
 // Those threshold values were chosen arbitrarily, more values should be tested if possible
-#define LOW_THRESHOLD 255
-#define HIGH_THRESHOLD 100
+#define HIGH_THRESHOLD 200
+#define LOW_THRESHOLD 100
 
 // The weights of the gaussian function were directly taken from the internet,
 // Would it be wise to test it for different values of the standard deviation ?
 // This will mainly affect the blur's effect and thus the final quality of the image...//
 #define KER_DIV 115
-const int8_t Gaus5x5 = { 2,  4,  5,  4, 2,
+const int8_t Gaus5x5[] = { 2,  4,  5,  4, 2,
                          4,  9, 12,  9, 4,
                          5, 12, 15, 12, 5,
                          4,  9, 12,  9, 4,
                          2,  4,  5,  4, 2 };
 
-const int8_t Kx = {-1, 0, 1,
+const int8_t Kx[] = {-1, 0, 1,
 				   -2, 0, 2,
 				   -1, 0, 1 };
 
-const int8_t Ky = {1, 2, 1,
+const int8_t Ky[] = {1, 2, 1,
 				   0, 0, 0,
 				   -1, -2, -1};
 
@@ -71,10 +71,10 @@ uint8_t canny_edge(){
 
 
 	//image conversion from RGB to grayscale//
-	uint8_t img_buffer = dcmi_get_last_image_ptr();
-	uint8_t img_temp_buffer;
+	uint8_t *img_buffer = dcmi_get_last_image_ptr();
+	uint8_t img_temp_buffer[(IM_LENGTH_PX*IM_HEIGHT_PX)/2] = {0};
 	uint16_t red, blue, green = 0;
-	for(uint8_t i; i < IM_LENGTH_PX * IM_HEIGHT_PX; i+=2){
+	for(uint16_t i; i < IM_LENGTH_PX * IM_HEIGHT_PX; i+=2){
 		red = (uint8_t)img_buffer[i/2] & 0xF8;
 		green = (uint8_t)((img_buffer[i/2] & 0x07) || (img_buffer[i/2+1] & 0xe0));
 		blue = (uint8_t)img_buffer[i/2+1] & 0x1F;
@@ -104,8 +104,7 @@ uint8_t canny_edge(){
 	// Application of the Sobel filters onto the image. //
 	// It will gives us the gradient intensity. //
 
-	float I_mag;
-	float theta;
+	float theta[(IM_LENGTH_PX*IM_HEIGHT_PX)/2]={0};
 
 	for(uint8_t x = XY_OFFSET_3x3; x < IM_LENGTH_PX-XY_OFFSET_3x3; ++x){
 		for(uint8_t y = XY_OFFSET_3x3; y < IM_HEIGHT_PX-XY_OFFSET_3x3; ++y){
@@ -139,7 +138,7 @@ uint8_t canny_edge(){
 	int8_t i,j = 0;
 	for(uint8_t x = XY_OFFSET_3x3; x < IM_LENGTH_PX-XY_OFFSET_3x3; ++x){
 		for(uint8_t y = XY_OFFSET_3x3; y < IM_HEIGHT_PX-XY_OFFSET_3x3; ++y){
-			position = x + (y * IM_LENGTH_PX)
+			position = x + (y * IM_LENGTH_PX);
 			if((theta[position] <= 22.5 && theta[position] >= -22.5) || (theta[position] <= -157.5 && theta[position] >= 157.5)){
 				i = img_buffer[position+1];
 				j = img_buffer[position-1];
@@ -165,9 +164,9 @@ uint8_t canny_edge(){
 	for (uint8_t x = 0; x < IM_LENGTH_PX; x++) {
 		for (uint8_t y = 0; y < IM_HEIGHT_PX; y++) {
 			position = x + (y * IM_LENGTH_PX);
-	        if (img_temp_buffer[src_pos] > HIGH_THRESHOLD) {
+	        if (img_temp_buffer[position] > HIGH_THRESHOLD) {
 	        	img_buffer[position] = HIGH_THRESHOLD;
-	        } else if (img_temp_buffer[src_pos] > LOW_THRESHOLD) {
+	        } else if (img_temp_buffer[position] > LOW_THRESHOLD) {
 	            img_buffer[position] = LOW_THRESHOLD;
 	        } else {
 	            img_buffer[position] = 0;
@@ -191,7 +190,6 @@ uint8_t canny_edge(){
 			else img_temp_buffer[position] = 0;
 		}
 	}
-	return img_temp_buffer;
+	return *img_temp_buffer;
 }
-
 
--- src/modules/image_processing.h
+++ src/modules/image_processing.h
@@ -1,10 +1,3 @@
-/*
- * image_processing.h
- *
- *  Created on: 5 avr. 2021
- *      Author: Yassine Bakkali
- */
-
 #ifndef SRC_MODULES_IMAGE_PROCESSING_H_
 #define SRC_MODULES_IMAGE_PROCESSING_H_
 
