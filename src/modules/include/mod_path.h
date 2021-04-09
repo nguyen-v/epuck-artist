42436889d01290becbf192514d59fa84a4441c10
Yassine Bakkali <ybakkali8@gmail.com>
2021-04-09 23:39:48 +0200

    Updated header file

--- src/modules/include/mod_path.h
+++ src/modules/include/mod_path.h
@@ -8,19 +8,15 @@
 #ifndef _MOD_PATH_H_
 #define _MOD_PATH_H_
 
-
-void path_labelling(void);
+uint8_t edge_scanning(uint8_t img_buffer, uint8_t count_lab);
+void path_labelling(uint8_t img_buffer);
 void path_planning(void);
 void path_optimization(void);
 void nearest_neighbour(void);
 
 uint8_t flatten(uint8_t old_img);
 uint8_t push_back(uint8_t temp_img_val, uint8_t new_img);
-uint8_t merge(uint8_t old_img);
-
-
-void unification();
-uint8_t find();
+uint8_t merge(uint8_t count_lab, uint8_t x, uint8_t y);
 uint16_t position(uint8_t pox_x, uint8_t pos_y);
 
 #endif /* _MOD_PATH_H_ */
