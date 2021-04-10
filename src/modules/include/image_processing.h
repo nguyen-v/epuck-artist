fd52ed2dbcf79f6f37e2d370ad5127dbf53ea482
Yassine Bakkali <ybakkali8@gmail.com>
2021-04-10 15:43:01 +0200

    Header modified

--- src/modules/include/image_processing.h
+++ src/modules/include/image_processing.h
@@ -1,6 +1,8 @@
 #ifndef SRC_MODULES_INCLUDE_IMAGE_PROCESSING_H_
 #define SRC_MODULES_INCLUDE_IMAGE_PROCESSING_H_
 
+enum colour{black, red, blue, green};
+
 void img_acquisition(void);
 uint8_t canny_edge(void);
 
