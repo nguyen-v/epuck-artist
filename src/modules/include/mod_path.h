2be996b91cb555ab051a7861de118c989bc35c0a
Yassine Bakkali <ybakkali8@gmail.com>
2021-04-16 18:11:04 +0200

    Path optimization and path order fully implemented

--- src/modules/include/mod_path.h
+++ src/modules/include/mod_path.h
@@ -8,15 +8,26 @@
 #ifndef _MOD_PATH_H_
 #define _MOD_PATH_H_
 
-typedef struct px_pos{
+#include <camera/po8030.h>
+#include <image_processing.h>
+#include <include/tools.h>
+#include <stdio.h>
+#include <string.h>
 
-	uint8_t pos_x;
-	uint8_t pos_y;
+#include "ch.h"
+#include "hal.h"
 
+struct edge_pos{
+
+	struct px_pos pos;
+	uint16_t index;
 };
 
+enum px_status{line = 0, edge = 1};
 
-typedef struct edge_track{
+enum edge_status{start = 0, end = 1};
+
+struct edge_track{
 
 	struct px_pos pos;
 	uint8_t label;
@@ -24,24 +35,26 @@ typedef struct edge_track{
 
 };
 
+struct path_motor{
 
-enum px_status{line = 0, edge = 1};
+	struct px_pos pos;
+	enum colour color;
+
+};
 
 
-uint8_t edge_scanning(uint8_t img_buffer, uint8_t count_lab);
-struct edge_track path_labelling(uint8_t img_buffer);
-void path_planning(void);
-uint16_t path_optimization(void);
-void nearest_neighbour(void);
 
-uint8_t flatten(uint8_t old_img);
-uint8_t push_back(uint8_t temp_img_val, uint8_t new_img);
-uint8_t merge(uint8_t count_lab, uint8_t x, uint8_t y);
-uint16_t position(uint8_t pox_x, uint8_t pos_y);
-uint16_t perpendicular_distance(struct px_pos start, struct px_pos end, struct px_pos point);
 
-void *array_concatenation(uint16_t *result, uint16_t *res1, uint16_t *res2, uint16_t size1, uint16_t size2);
-void save_pos(edge_track *pos, uint8_t x, uint8_t y, uint8_t label,px_status start_end, uint8_t k);
 
+struct path_motor path_planning(uint8_t *img_buffer);
+void edge_scanning(uint8_t *img_buffer, uint8_t *count_lab);
+void path_labelling(uint8_t *img_buffer);
+void edge_tracing(struct edge_track *contours, struct edge_pos *edges);
+uint16_t path_optimization(struct edge_track *contours, uint16_t size, struct edge_track* dest, uint16_t destlen);
+void nearest_neighbour(struct edge_pos *edges, uint16_t *edge_index);
+
+void save_pos(struct edge_track *pos, uint8_t x, uint8_t y, uint8_t label, enum px_status start_end, uint8_t k);
+void save_edge_pos(struct edge_pos *pos, uint8_t x, uint8_t y, uint8_t curve_index, uint8_t this_index);
+void swap(struct px_pos *edges1, struct px_pos *edges2);
 
 #endif /* _MOD_PATH_H_ */
--- src/modules/mod_path.c
+++ src/modules/mod_path.c
@@ -11,13 +11,12 @@
 
 #include <chprintf.h>
 #include <usbcfg.h>
-#include <camera/po8030.h>
-#include <mod_communication.h>
-#include <include/image_processing.h>
 #include <include/mod_path.h>
 
 #define IM_LENGTH_PX 100
-#define IM_HEIGHT_PX 90
+#define IM_HEIGHT_PX 100
+#define INIT_ROBPOS_PX 50
+#define INIT_ROBPOS_PY 50
 
 // This value dictates the efficacity of the path optimization algorithm.
 // A higher value would give us a path with sharp turns while a lower value would give us curved turns
@@ -29,7 +28,9 @@ const uint8_t dy[] = {-IM_LENGTH_PX, 0, -IM_LENGTH_PX, -IM_LENGTH_PX};
 static uint8_t label[IM_LENGTH_PX*IM_HEIGHT_PX] = {0};
 static uint8_t count_lab[IM_LENGTH_PX*IM_HEIGHT_PX] = {0};
 
-
+static uint16_t size_contours = 0;
+static uint16_t size_edges = 0;
+static uint16_t opt_size_contours = 0;//Those static values should be utilized in order to alloc the necessary memory for the countour and the edge arrays
 
 
 /**
@@ -37,11 +38,11 @@ static uint8_t count_lab[IM_LENGTH_PX*IM_HEIGHT_PX] = {0};
  * 				This algorithm uses a 8 neighbour checking system and raster scanning to attribute labels to defined edges.
  *
  */
-
-uint8_t edge_scanning(uint8_t *img_buffer, uint8_t *count_lab){
+void edge_scanning(uint8_t *img_buffer, uint8_t *count_lab){
 
 	uint16_t pos = 0;
 	uint8_t counter = 1;
+
 	for(uint8_t x=1; x<IM_LENGTH_PX;++x){
 		for(uint8_t y=1; y<IM_HEIGHT_PX;++y){
 			pos = position(x,y);
@@ -50,13 +51,13 @@ uint8_t edge_scanning(uint8_t *img_buffer, uint8_t *count_lab){
 					if(img_buffer[pos + dx[1] + dy[1]]){
 						label[pos] = label[pos + dx[1] + dy[1]];
 						if(img_buffer[pos + dx[2] + dy[2]])
-							merge(&count_lab,label[pos],label[pos + dx[2] + dy[2]]);
+							merge(*count_lab,label[pos],label[pos + dx[2] + dy[2]]);
 					}
 					else{
 						if(img_buffer[pos + dx[2] + dy[2]]){
 						label[pos] = label[pos + dx[2] + dy[2]];
 						if(img_buffer[pos + dx[3] + dy[3]])
-							merge(&count_lab,label[pos],label[pos + dx[3] + dy[3]]);
+							merge(*count_lab,label[pos],label[pos + dx[3] + dy[3]]);
 						}
 						else{
 							if(img_buffer[pos + dx[3] + dy[3]]){
@@ -77,17 +78,31 @@ uint8_t edge_scanning(uint8_t *img_buffer, uint8_t *count_lab){
 			}
 		}
 	}
-	return counter;
 }
 
 
-struct edge_track edge_tracing(){
 
-	uint8_t diag_px_priority, temp_x, temp_y, next_x, next_y, last_x, last_y, k, j, used_label_trace = 0;
-	uint8_t start_end, tracing_progress, traced_label[100], next_px, end_found, lap_found = 0;
-	uint16_t priority, pos = 0;
+
+void path_labelling(uint8_t *img_buffer){
+
+	edge_scanning(img_buffer,count_lab);
+	flatten(*count_lab);
+	for(uint8_t x=0 ;x < IM_LENGTH_PX; x++){
+		for(uint8_t y=0 ; y < IM_HEIGHT_PX; y++){
+			label[position(x,y)]=count_lab[position(x,y)];
+		}
+	}
+}
+
+
+
+void edge_tracing(struct edge_track *contours, struct edge_pos *edges){
+
+	uint8_t diag_px_priority, temp_x, temp_y, next_x, next_y, last_x, last_y,j, k = 0;
+	uint8_t start_end, tracing_progress, next_px = 0;
+	uint8_t l = 1;
+	uint16_t pos = 0;
 	enum px_status status = 0;
-	struct edge_track contours[1000] ={0};
 	for(uint8_t x=0 ;x < IM_LENGTH_PX;x++){
 		for(uint8_t y=0 ; y < IM_HEIGHT_PX;y++){
 			//if(label[position(x,y)]){
@@ -172,40 +187,33 @@ struct edge_track edge_tracing(){
 //				start_end += (label[position(x+1,y+1)] == label[position(x,y)]) && start_end < 3 ? 1 : 0;
 
 				if(start_end){
-					if(start_end == 1)
+					if(start_end == 1){
 						status = edge;
-					else if(start_end == 2) status = line;
-					else break; // A chprintf should be added here in case something abnormal happens with the image processing algorithm, or the labeling process
+						save_edge_pos(edges, x, y, k, l);
+						++l;
+						++j;
+						++size_edges;
+					} else{
+						if(start_end == 2) status = line;
+					}
+					++size_contours;
 				}
+				save_pos(contours, x, y, label[pos],status,k);
+				++k;
 
-				save_pos(*contours, x, y, label[pos],status,k);
 				label[position(x,y)] = 0; // MAGIC VALUE -> Transforms label into background
 				x = next_x;
 				y = next_y;
 				pos = position(x,y);
-				++k;
 				next_px = FALSE;
-				if(status == edge){
-					++j;
-					if(j == 2){
-						x = temp_x;
-						y = temp_y;
-						j = 0;
-					}
+				start_end = 0;
+
+				if(j == 2){
+					x = temp_x;
+					y = temp_y;
+					j = 0;
 				}
 			}
-		}
-	}
-}
-
-void path_labelling(uint8_t *img_buffer){
-
-	uint8_t counter = 0;
-	counter = edge_scanning(&img_buffer,count_lab);
-	flatten(count_lab,counter);
-	for(uint8_t x=0 ;x < IM_LENGTH_PX;x++){
-		for(uint8_t y=0 ; j < IM_HEIGHT_PX;j++){
-			label[position(x,y)]=p[position(x,y)];
 		}
 	}
 }
@@ -214,170 +222,208 @@ void path_labelling(uint8_t *img_buffer){
  * @brief This is an implementation of the recursive Douglas-Pucker algorithm which significantly reduces the total number of points
  * 		  in a contour.
  */
-uint16_t path_optimization(struct edge_track *contours,  uint16_t start, uint16_t end){
+uint16_t path_optimization(struct edge_track *contours, uint16_t size, struct edge_track* dest, uint16_t destlen){
 
+	uint16_t index = 0;
 	float dmax = 0;
 	float distance = 0;
-	uint16_t index = 0;
-	for(uint16_t i = start; i <= end  ; ++i)
-	{
 
-		distance = perpendicular_distance(contours[start], contours[end], contours[i]);
+	for(uint16_t i = 1; i + 1 < size  ; ++i)
+	{
+		distance = perpendicular_distance(contours[0].pos, contours[size-1].pos, contours[i].pos);
 		if(distance > dmax) {
 			index = i;
 			dmax = distance;
 		}
 	}
 
-	uint16_t full_size = (end-start);
-	uint16_t *resultList = malloc((full_size)*sizeof(uint16_t));
-	uint16_t res1_size = (index - start);
-	uint16_t *recResults1 = malloc(res1_size*sizeof(uint16_t));
-	uint16_t res2_size = (end-index);
-	uint16_t *recResults2 = malloc(res2_size*sizeof(uint16_t));
-
-	// Do not forget to find a way to free them in the main path_finding function !!!! //
-
 	if(dmax > EPSILON){
 		// Appel récursif
-		recResults1 = path_optimization(&contours, start, index);
-		recResults2 = path_optimization(&contours, index, end);
-		resultList = array_concatenation(resultList,recResults1,recResults2,res1_size,res2_size);
+		uint16_t n1 = path_optimization(contours, index + 1, dest, destlen);
+		if (destlen >= n1 - 1){
+			destlen -= n1 - 1;
+			dest += n1 - 1;
+		} else {
+			destlen = 0;
+		}
+		uint16_t n2 = path_optimization(contours + index, size-index, dest, destlen);
+		return n1 + n2 - 1;
 	}
-	else {
-		resultList = array_concatenation(resultList,contours[0],contours[end],1,1);
+	if(destlen >= 2) {
+		dest[0] = contours[0];
+		dest[1] = contours[size - 1];
 	}
-	return resultList;
+	return 2;
 }
 
 
 /**
  * @brief   Implementation of the nearest neighbour algorithm. It depends on the position of the start and the end points.
+ * 			WHO CARES ABOUT WHAT IT DOES ? IT DOESN'T WORK NO MATTER HOW I IMPLEMENT IT FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK...
  */
-void nearest_neighbour(void){
 
 
+void nearest_neighbour(struct edge_pos *edges, uint16_t *edge_index){
 
+	uint16_t visited[size_edges] = {0};
+	visited[0] = 1;
 
+	uint16_t dmin,d = 0;
+	uint16_t algo_finished = 0;
+	uint16_t j = 1;
 
+	uint16_t counter_temp = (size_edges - 1) / 2;
 
-}
+	While(!algo_finished){
 
+		uint16_t k = 0;
 
-/**
- * @brief
- */
-void path_planning(void){
+		uint16_t index = 0;
+		struct edge_pos temp = {0};
 
+		for(uint16_t i = 0; i < size_edges; ++i){
 
+			if(!visited[i]){
 
-
-
-
-}
-
-
-
-
-/**
- * @brief				This function transforms an array of arrays into a single array of 1 dimension elements
- */
-uint8_t flatten(uint8_t *old_img){
-
-	uint8_t temp_img[] = {0};
-	if(sizeof(old_img) == 1)
-		return old_img;
-	uint8_t new_img[] = {0};
-	for(uint8_t i=0; i<=sizeof(old_img); i++){
-			temp_img = flatten(old_img(i));
-			for(uint8_t j = 0; j<= sizeof(temp_img); ++j){
-				new_img = push_back(temp_img[j], new_img);
+				d = two_point_distance(edges[k], edges[i]);
+				if(dmin == 0)
+					dmin = d;
+				if(d <= dmin){
+					dmin = d;
+					index = i;
+				}
 			}
 		}
-	return new_img;
-}
-
-// There is no way in hell I'm playing with all of that malloc/realloc/free bullshit
-/**
- * @brief				A rewriting of the C++ function "push_back()"
- */
-uint8_t push_back(uint8_t temp_img_val, uint8_t *new_img){
-
-	uint8_t newer_img[sizeof(new_img)/sizeof(uint8_t) + 1] = {0};
-
-	for(uint8_t i = 0; i < sizeof(new_img)/sizeof(uint8_t); ++i)
-	newer_img[i] = new_img[i];
-
-	newer_img[sizeof(new_img)/sizeof(uint8_t) + 1] = temp_img_val;
-	return newer_img;
-}
-
-//n
-/**
- * @brief		This function gives the same label to 2 pixels with different labels
- */
-void merge(uint8_t *count_lab, uint8_t x, uint8_t y){
-
-	while(count_lab[x]!=count_lab[y]){
-		if(count_lab[x]>count_lab[y])
-			count_lab[rootx]=count_lab[rooty];
-		else
-			count_lab[rooty]=count_lab[rootx];
+		if(index%2==0){
+			//swap(edges[j+1],edges[index]);
+			swap(edge_index[j+1],edge_index[index]);
+			visited[j+1] = 1;
+			//swap(edges[j],edges[index-1]);
+			swap(edge_index[j],edge_index[index-1]);
+			visited[j] = 1;
+			k = index - 1;
+		}
+		else {
+			//swap(edges[j],edges[index]);
+			swap(edge_index[j],edge_index[index]);
+			visited[j] = 1;
+			//swap(edges[j+1],edges[index+1]);
+			swap(edge_index[j+1],edge_index[index+1]);
+			visited[j] = 1;
+			k = index + 1;
+		}
+		if(counter_temp == size_edges){
+			algo_finished = 1;
+		}
+		j += 2;
 	}
 }
 
 
-uint16_t position(uint8_t pox_x, uint8_t pos_y){
-
-	uint16_t position = pos_x + pos_y*IM_LENGTH_PX;
-	return position;
-
+void swap(struct px_pos *edges1, struct px_pos *edges2){
+	uint16_t temp = 0;
+	temp = edges1;
+	edges1 = edges2;
+	edges2 = temp;
 }
 
-void save_pos(struct edge_track *pos, uint8_t x, uint8_t y, uint8_t label,px_status start_end, uint8_t k){
-	pos[k]->pos->pos_x= x;
-	pos[k]->pos->pos_y = y;
-	pos[k]->label = label;
-	pos[k]->start_end = start_end;
-}
 
-uint16_t perpendicular_distance(struct px_pos start, struct px_pos end, struct px_pos point){
 
-	uint8_t line_x = end.pos_x - start.pos_x;
-	uint8_t line_y = end.pos_y - start.pos_y;
 
-	uint8_t vec_x = point.pos_x - start.pos_x;
-	uint8_t vec_y = point.pos_y - start.pos_y;
+/**
+ * @brief			This functions utilizes all the other functions defined above in order to implement the complete path finding program
+ */
 
-	uint8_t vec_x2 = point.pos_x - end.pos_x;
-	uint8_t vec_y2 = point.pos_x - end.pos_y;
+struct path_motor path_planning(uint8_t *img_buffer){
 
-	uint16_t dot_prod1 = (line_x * vec_x + line_y * vec_y);
-	uint16_t dot_prod2 = (line_x * vec_x2 + line_y * vec_y2);
+	struct edge_track *contours[5000] ={0};			// Unless we find a way to quantify it, the size of this array will be chosen arbitrarily
+	struct edge_pos *edges[5000] = {0};				// The same goes for this array
+													// Though both can be realocated later on...
+	edges[0]->pos.pos_x = INIT_ROBPOS_PX;
+	edges[0]->pos.pos_y = INIT_ROBPOS_PY;
+	++size_edges;
 
-	uint16_t distance = 0;
+	//----------- This part labels the contours and extracts them into one single array conveniently named "Contours" ---------------//
 
-	if (dot_prod1 > 0)
-		distance = arm_sqrt_q15(vec_x2*vec_x2 + vec_y2*vec_y2);
-	else if(dot_prod2 < 0)
-		distance =  arm_sqrt_q15(vec_x*vec_x + vec_y*vec_y);
-	else {
-		uint16_t mod = arm_sqrt_q15(line_x*line_x + line_y*line_y);
-		distance = abs(line_x*vec_y - line_y*vec_x) / mod;
+	uint16_t result_size = 0;
+	path_labelling(img_buffer);
+	edge_tracing(contours, edges);
+	edges = (struct edge_pos*)realloc(edges, size_edges*sizeof(struct edge_pos));
+	contours = (struct edge_track*)realloc(contours, size_contours*sizeof(struct edge_track));  //We realocate the proper memory weight for each array;
+
+
+	//-------- This part separates all the contours and optimizes them separately before reuniting them together -----------//
+
+	uint16_t total_size = 0;
+	uint16_t contours_size = 0;
+
+	for(uint8_t i = 0; i < size_edges/2; ++i){
+		uint16_t k = 0;
+		uint16_t length = edges[i*2 + 1]->index - edges[i*2]->index + 1;
+		struct edge_track res_out[length] = {0};
+		struct edge_track *new_contour = (struct edge_track*)malloc(length*sizeof(struct edge_track));
+		for(uint8_t n = 0; n < length; ++n){
+			new_contour[n] = contours[contours_size + n];
+		}
+
+		contours_size += length;
+		uint16_t final_size = path_optimization(new_contour,length, res_out,length);
+
+		for(uint8_t j = total_size; j < total_size + final_size; ++j){
+				contours[j] = res_out[k];
+				++k;
+		}
+		total_size += final_size;
+		free(new_contour);
 	}
-return distance;
+	contours = (struct edge_track*)realloc(contours,total_size*sizeof(struct edge_track));
+
+
+	//-------------------------------- This part decides the final path of the robot -----------------------//
+
+	// We must first find the position of each and every edge again
+	uint16_t edge_index[size_edges] = {0};
+	uint16_t k = 1;
+	edge_index[0] = 0;
+	edge_index[size_edges - 1] = total_size - 1;
+	for(uint8_t i = 1; i < size_edges-1; ++i){
+		if(contours[i]->label != contours[i-1]->label){
+			edge_index[k] = i-1;
+			edge_index[++k] = i;
+			++k;
+		}
+	}
+
+	//---------------------- This part reorganizes the edges' positions. With this, we know how to read the contours table -----------//
+	nearest_neighbor(edges, edge_index);
+
+	for(uint8_t i = 0; i < total_size; ++i){
+
+
+
+	}
+	return path;
 }
 
 
 
-void *array_concatenation(uint16_t *result, uint16_t *res1, uint16_t *res2, uint16_t size1, uint16_t size2){
-
-	    memcpy(result, res1, size1*sizeof(uint16_t));
-	    memcpy(result + size1, res2, size2*sizeof(uint16_t));
-	    return result;
-
-
+void save_pos(struct edge_track *pos, uint8_t x, uint8_t y, uint8_t label, enum px_status start_end, uint8_t k){
+	pos[k].pos.pos_x= x;
+	pos[k].pos.pos_y = y;
+	pos[k].label = label;
+	pos[k].start_end = start_end;
 }
+
+
+void save_edge_pos(struct edge_pos *pos, uint8_t x, uint8_t y, uint8_t curve_index, uint8_t this_index){
+	pos[this_index].pos.pos_x = x;
+	pos[this_index].pos.pos_y = y;
+	pos[this_index].index = curve_index;
+}
+
+
+
+
 
 
 
