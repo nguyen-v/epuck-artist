e066d0bba9c13c725473a8eb5c5ac40d2c6357bf
Vincent <vincen7.nguyen@gmail.com>
2021-04-24 17:27:15 +0200

    Added headers and support for 2 argument commands

--- python/epuck-communication.py
+++ python/epuck-communication.py
@@ -10,6 +10,8 @@ import numpy as np
 import struct
 from threading import Thread
 
+from PIL import Image # read image
+
 # ===========================================================================
 #  Module constants.                                                         
 # ===========================================================================
@@ -34,9 +36,44 @@ COMMANDS = (
     'B'     ,   # BEGIN CALIBRATION
     'G'     ,   # GET DATA
     'D'     ,   # DRAW
-    'I'         # INTERACTIVE
+    'I'     ,   # INTERACTIVE
+    'H'     ,   # HOME
+    'V'     ,   # VALIDATE
 )
 
+# associate an index to each command
+CMD_INDEX = {
+    'R' : 0    ,
+    'P' : 1    ,   
+    'C' : 2    ,  
+    'B' : 3    ,   
+    'G' : 4    ,   
+    'D' : 5    ,   
+    'I' : 6    ,   
+    'H' : 7    , 
+    'V' : 8    
+}
+
+CMD_HEADER = [b'' for x in range(len(COMMANDS))]
+CMD_HEADER[CMD_INDEX['V']] = b'LEN'
+CMD_HEADER[CMD_INDEX['G']] = b'MOVE'
+
+# commands that need a second argument
+COMMANDS_TWO_ARGS = (
+    'V'     ,   # VALIDATE
+)
+
+# associate a command to an index in the SECOND_ARG_LIMIT matrix
+CMD_TWO_ARGS_INDEX = {
+    'V' : 0
+}
+
+# create a matrix of size len(COMMANDS_TWO_ARG) x 2
+# that will contain the lower and upper bounds of second argument
+SECOND_ARG_LIMIT = [0 for x in range(2) for y in range(len(COMMANDS_TWO_ARGS))]
+
+# assign lower and upper bounds
+SECOND_ARG_LIMIT[CMD_TWO_ARGS_INDEX['V']] = [0, 150] # in mm
 
 # ===========================================================================
 #  Threads.                                                         
@@ -73,6 +110,84 @@ COMMANDS = (
 #     # def run(self):
 
 #     #     while(self.alive):
+
+def receive_image(ser):
+    state = 0
+
+    while(state != 5):
+        #reads 1 byte
+        c1 = ser.read(1)
+        #timeout condition
+        if(c1 == b''):
+            return [];
+
+        if(state == 0):
+            if(c1 == b'S'):
+                state = 1
+            else:
+                state = 0
+        elif(state == 1):
+            if(c1 == b'T'):
+                state = 2
+            elif(c1 == b'S'):
+                state = 1
+            else:
+                state = 0
+        elif(state == 2):
+            if(c1 == b'A'):
+                state = 3
+            elif(c1 == b'S'):
+                state = 1
+            else:
+                state = 0
+        elif(state == 3):
+            if(c1 == b'R'):
+                state = 4
+            elif (c1 == b'S'):
+                state = 1
+            else:
+                state = 0
+        elif(state == 4):
+            if(c1 == b'T'):
+                state = 5
+            elif (c1 == b'S'):
+                state = 1
+            else:
+                state = 0
+
+    print("Receiving image")
+
+
+
+    toRead = 4000
+    rcv_buffer = b''
+    # print(rcv_buffer)
+    # print(len(rcv_buffer))
+    #reads the data
+    i = 0
+    # while i < toRead:
+    #     print(ser.read())
+    #     i +=1
+
+# WORKING 90X100 rgb
+    # rcv_buffer += ser.read(toRead)
+    # rcv_buffer += ser.read(toRead)
+    # rcv_buffer += ser.read(toRead)
+    # rcv_buffer += ser.read(toRead)
+    # rcv_buffer += ser.read(2000)
+
+    rcv_buffer += ser.read(toRead)
+    rcv_buffer += ser.read(toRead)
+    rcv_buffer += ser.read(1000)
+
+    new_buffer = bytearray(len(rcv_buffer))
+    # new_buffer[0::2] = rcv_buffer[1::2]
+    # new_buffer[1::2] = rcv_buffer[0::2]
+    # im = Image.frombytes("RGB", (100, 90), bytes(new_buffer), "raw", "BGR;16")
+    im = Image.frombytes("L", (100, 90), bytes(rcv_buffer), "raw")
+    nameimg ="Image"
+    im.show(title=nameimg)
+    im.save("C:/Users/41786/Desktop/Projects/BA-6/SE/img" + ".png", "PNG")
 
 def parse_arg():
     port = SERIAL_PORT_DEFAULT
@@ -120,27 +235,59 @@ def send_command(ser, command):
     if command not in COMMANDS:
         print("Invalid command: " + str(command))
     else:
+        cmd_header = CMD_HEADER[CMD_INDEX[command]]
+        second_arg = b''
+        if command in COMMANDS_TWO_ARGS:
+            try:
+                lower_bound = SECOND_ARG_LIMIT[CMD_TWO_ARGS_INDEX[command]][0]
+                upper_bound = SECOND_ARG_LIMIT[CMD_TWO_ARGS_INDEX[command]][1]
+                second_arg = np.uint8(input("Enter an integer between %d and %d: "
+                % (lower_bound, upper_bound)))
+                if (lower_bound < second_arg <  upper_bound):
+                    pass
+                else:
+                    print("Second argument must be between %d and %d: "
+                    % (lower_bound, upper_bound))
+                    return
+            except ValueError:
+                print("Second argument must be an integer")
+                return
         try:
-            # ser.reset_input_buffer()
-            # ser.reset_output_buffer()
             ser.reset_input_buffer()
             ser.reset_output_buffer()
+
+            # send command
             ser.write(b'CMD')
             ser.write(command.encode()) # send command as bytes (utf-8)
+
+            # send header and second argument
+            ser.write(cmd_header)
+            if second_arg != b'':
+                ser.write(struct.pack('B', second_arg))
 
         except serial.SerialException:
             print("Error occured when sending command. "
             "Connection to e-puck lost.")
         time.sleep(2) # wait for the e-puck to properly process the command
 
+        if command == 'G':
+            send_move_data(ser)
+
 def get_data():
     # data_color = np.array([0, 4, 6], dtype = np.uint8)                               # rewrite
-    data_color = np.array([0,3,1,5,1,1,7,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], dtype = np.uint8)                               # rewrite
+    data_color = np.array([100,3,1,5,1,1,7,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], dtype = np.uint8)                               # rewrite
     # data_pos = np.array([[430, 0]], dtype = np.uint16)
     # data_pos = np.array([[512, 0], [612, 0], [612, 50], [512, 50], [512, 0]], dtype = np.uint16) # 2 squares
-    data_pos = np.array([[512, 0], [463, 0],[467, 26], [461, 49], [435, 89],[415, 138], [404, 158], [417, 164], [448, 139], [450, 104], [449, 154], [455, 187], [481, 186], [477, 163], [474, 284], [487, 294], [500, 289], [502, 268], [482, 268], [481, 263], [481, 285], [494, 285], [481, 263],[494, 269], [501, 267], [508, 188], [516, 160], [509, 188], [534, 186],[553, 152], [541, 175], [568, 157], [568, 127], [542, 59], [544, 0], [512, 0]], dtype = np.uint16)
+    # data_pos = np.array([[512, 0], [463, 0],[467, 26], [461, 49], [435, 89],[415, 138], [404, 158], [417, 164], [448, 139], [450, 104], [449, 154], [455, 187], [481, 186], [477, 163], [474, 284], [487, 294], [500, 289], [502, 268], [482, 268], [481, 263], [481, 285], [494, 285], [481, 263],[494, 269], [501, 267], [508, 188], [516, 160], [509, 188], [534, 186],[553, 152], [541, 175], [568, 157], [568, 127], [542, 59], [544, 0], [512, 0]], dtype = np.uint16)
     # data_pos = np.array([[512, 0], [562, 0],[562, 50], [512, 50], [512, 0],[512, 100], [562, 100], [562, 50], [562, 0], [512, 0]], dtype = np.uint16)
     # data_pos = np.array([[513, 0], [578, 14], [627,48 ], [657, 96], [669, 154], [657,213 ], [622, 263], [571, 297], [516, 307], [454, 296], [404, 261], [372,210 ], [360, 149], [373, 93], [407,45 ], [447, 15], [512, 0]], dtype = np.uint16) # circle
+     # data_pos = np.array([[512, 0], [562, 0],[562, 50], [512, 50], [512, 0],[512, 100], [562, 100], [562, 50], [562, 0], [512, 0]], dtype = np.uint16)
+    # data_pos = np.array([[512, 0], [600, 0]], dtype = np.uint16) # long line
+    # data_pos = np.array([[512, 0],[512, 30], [512, 0], [612, 0],[512, 0], [412, 0], [512, 0]], dtype = np.uint16) # y test
+    data_pos = np.array([[512, 0],[512, 100], [512, 200],[612, 200], [712, 200], [712, 100],[712, 0],[612, 0],[512, 0]], dtype = np.uint16) # square test
+    # data_pos = np.array([[512, 0],[512, 150]], dtype = np.uint16) # going down
+    # data_pos = np.array([[480, 0]], dtype = np.uint16) # y test
+    
     # data_pos = np.array([[512, 0], [512, 5], [517,5 ], [517, 0], [512, 0]], dtype = np.uint16) # circle
     # data_pos = np.array([[512, 0], [570, 0], [512, 0], [560, 20], [512, 0], [550, 40],[512, 0],[540, 70],[512, 0],[530, 80],[512, 0],[520, 90],[512, 0],[512, 100],[512, 0]], dtype = np.uint16)
 
@@ -168,7 +315,7 @@ def send_move_data(ser):
     print("Sending move data to the e-puck...")
     ser.reset_input_buffer()
     ser.reset_output_buffer()
-    ser.write(b'MOVE')
+    # ser.write(b'MOVE')
     ser.write(struct.pack('<h',size[0]))
 
     ser.write(send_buffer)
@@ -188,10 +335,10 @@ def main():
     ser = connect_to_epuck(port)
 
     while True:
-        command = input("Type a command")
+        command = input("Type a command: ")
         send_command(ser, command)
-        if command == 'G':
-            send_move_data(ser)
+        # elif command == 'B':
+        #     # receive_image(ser)
 if __name__=="__main__":
     main()
     
