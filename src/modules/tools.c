23df49155dfc6ba539138f3f1f10b231b2c240d2
Vincent <vincen7.nguyen@gmail.com>
2021-05-05 23:41:46 +0200

    Added position function

--- src/modules/tools.c
+++ src/modules/tools.c
@@ -1,19 +1,30 @@
-/*
- * tools.c
- *
- *  Created on: 12 avr. 2021
- *      Author: YassineBakkali
+/**
+ * @file    tools.c
+ * @brief   Definitions of useful distance calculation and position functions.
  */
+
+
+// C standard header files
 #include <math.h>
 #include <stdint.h>
 
+// Module headers
 #include <tools.h>
+#include <mod_img_processing.h>
 
+/*===========================================================================*/
+/* Module exported functions.                                                */
+/*===========================================================================*/
 
+uint16_t position(uint8_t pos_x, uint8_t pos_y){
+
+	uint16_t position = pos_x + (uint16_t)pos_y*IM_LENGTH_PX;
+	return position;
+}
 
 float perpendicular_distance(struct cartesian_coord start, struct cartesian_coord end, struct cartesian_coord point){
 
-	int32_t line1 = start.x - end.x;
+	int32_t line1 = start.x - end.x; //int32 to avoid underflow
 	int32_t line2 = start.y - end.y;
 
 	int32_t vec_x = point.x - start.x;
@@ -26,7 +37,6 @@ float perpendicular_distance(struct cartesian_coord start, struct cartesian_coor
 
 return distance;
 }
-
 
 
 float two_point_distance(struct cartesian_coord point1, struct cartesian_coord point2){
