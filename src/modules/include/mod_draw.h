31e1004e3817c42fd76e9b5d6c632d5dddbbd486
Vincent <vincen7.nguyen@gmail.com>
2021-05-06 23:14:03 +0200

    Added pen lifting after drawing is finished, cleaned up comments

--- src/modules/include/mod_draw.h
+++ src/modules/include/mod_draw.h
@@ -1,43 +1,86 @@
 /**
  * @file    mod_data.h
- * @brief   Header for drawing module.
+ * @brief   External declarations of drawing module.
  */
 
 #ifndef _MOD_DRAW_H_
 #define _MOD_DRAW_H_
 
 /*===========================================================================*/
-/* Module data structures and types.                                         */
+/* Exported constants                                                        */
 /*===========================================================================*/
 
-
+#define IM_MAX_HEIGHT     200 // px
+#define IM_MAX_WIDTH      200 // px
 
 /*===========================================================================*/
 /* External declarations.                                                    */
 /*===========================================================================*/
 
-//void right_motor_set_length(uint16_t step, int16_t speed);
-//
-//void left_motor_set_length(uint16_t step, int16_t speed);
-
-void draw_set_init_length(float y_length);
+/**
+ * @brief            Stops stepper motor and resets position
+ * @return           none
+ */
 void draw_reset(void);
+
+/**
+ * @brief            Create drawing thread
+ * @return           none
+ */
 void draw_create_thd(void);
+
+/**
+ * @brief            Stop drawing thread
+ * @return           none
+ */
 void draw_stop_thd(void);
+
+/**
+ * @brief            Pause drawing thread
+ * @return           none
+ */
 void draw_pause_thd(void);
+
+/**
+ * @brief            Resume drawing thread
+ * @return           none
+ */
 void draw_resume_thd(void);
 
+/**
+ * @brief            Returns current thread state
+ * @return           True if currently drawing, false otherwise
+ */
+bool draw_get_state(void);
+
+/**
+ * @brief            Set initial distance between e-puck and the line between
+ *                   2 thread attach points
+ * @return           none
+ */
+void draw_set_init_length(float y_length);
+
+/**
+ * @brief            Get the average current wire length in steps
+ * @return           none
+ */
 uint16_t draw_get_length_av_current(void);
 
-uint16_t draw_get_length_av_next(uint16_t x, uint16_t y);
 /**
- * @brief			Moves the e-puck to specified XY coordinates.
- * @note			(0,0) is at the top-left of the canvas.
- * @param[in] x 	x coordinate, ranging from (0-X_RESOLUTION)
- * @param[in] y 	y coordinate, ranging from ?
- * @return			none
+ * @brief            Get the average length needed to reach xy coordinates
+ * @param[in]   x    x coordinate in pixels
+ * @param[in]   y    y coordinate in pixels
+ * @return           Average length in steps
+ */
+uint16_t draw_get_length_av_next(uint16_t x, uint16_t y);
+
+/**
+ * @brief            Moves the e-puck to specified XY coordinates
+ * @note             (0,0) is at the top-left of the canvas
+ * @param[in]   x    x coordinate in pixels
+ * @param[in]   y    y coordinate in pixels
+ * @return           Average length in steps to reach desired coordinates
  */
 void draw_move(uint16_t x, uint16_t y);
-//void motor_set_step(int32_t step_left, int32_t step_right, uint16_t speed);
 
-#endif
+#endif /* _MOD_DRAW_H_ */
--- src/modules/mod_draw.c
+++ src/modules/mod_draw.c
@@ -7,18 +7,20 @@
 // C standard header files
 
 #include <stdint.h>
-#include <math.h>
 #include <stdlib.h>
+#include <math.h>
 
 // ChibiOS headers
 
 #include "hal.h"
 #include "ch.h"
+#include <usbcfg.h>
+#include "chprintf.h"
+#include "chschd.h"
+
+// e-puck 2 main processor headers
+
 #include <motors.h>
-#include <usbcfg.h>		// usb debug messages
-#include "chprintf.h" 	// usb debug messages
-#include "chschd.h" 	// thread functions
-//#include "arm_math.h"	// slower than math.h functions...
 
 // Module headers
 
@@ -31,49 +33,25 @@
 /* Module constants.                                                         */
 /*===========================================================================*/
 
-//#define X_RESOLUTION			1024
-//#define PI						3.14159265358979f
-//// e-puck
-//#define SPOOL_DISTANCE     		8.0f    // cm
-//#define SPOOL_DIAMETER     		2.1f 	 // cm
-//#define SPOOL_PERIMETER			(PI*SPOOL_DIAMETER) // cm
-//#define NSTEP_ONE_TURN			1000.0f
-//
-//// field geometry
-//#define SUPPORT_DISTANCE 		48.0f // cm
-//#define MARGIN					(SPOOL_DISTANCE*2) // cm
-//
-//#define CM_TO_STEP				(NSTEP_ONE_TURN/SPOOL_PERIMETER)
-//
-//// in steps
-//#define SPOOL_DISTANCE_ST  		((uint16_t)(SPOOL_DISTANCE*CM_TO_STEP))
-//#define SUPPORT_DISTANCE_ST  	((uint16_t)(SUPPORT_DISTANCE*CM_TO_STEP))
-//#define MARGIN_ST 				((uint16_t)(MARGIN*CM_TO_STEP))
-//
-//#define CART_TO_ST				((float)(SUPPORT_DISTANCE_ST-2*MARGIN_ST)/X_RESOLUTION)
+#define MAX_SPEED              250    // steps/s
 
-#define MIN_SPEED				80 // step/s
-#define MAX_SPEED 				250 // steps/s
+#define STEP_THRESHOLD         5      // defines how close we should get to
+								       // goal length in steps
 
-#define STEP_THRESHOLD			5 // defines how close we should get to
-								  // goal length in steps
-
-#define TIME_SLEEP_MIN			20 // ms motors dont have time to react if too low
+#define TIME_SLEEP_MIN         20     // ms motors dont have time to react if
+                                       // too low
+#define DEFAULT_HEIGHT         100.0f // cm
 
 /*===========================================================================*/
 /* Module local variables.                                                   */
 /*===========================================================================*/
 
-
-//static float pos_y_initial = 0;	// cm
-static uint16_t y0_st = 100.0*CM_TO_STEP;
+static uint16_t y0_st = DEFAULT_HEIGHT*CM_TO_STEP;
 static uint16_t x0_st = (SUPPORT_DISTANCE_ST-SPOOL_DISTANCE_ST)/2.;
-static uint16_t len0_st = 0; //steps
+static uint16_t len0_st = 0;
 
 static bool is_drawing = false;
 static bool is_paused = false;
-
-//static cartesian_coord current_pos = {X_RESOLUTION/2, 0};
 
 /*===========================================================================*/
 /* Module thread pointers.                                                   */
@@ -82,61 +60,16 @@ static bool is_paused = false;
 static thread_t* ptr_draw;
 
 /*===========================================================================*/
-/* Module mutexes, semaphores.                                               */
-/*===========================================================================*/
-//MUTEX_DECL(serial_mtx);
-
-/*===========================================================================*/
-/* Module threads.                                                   		 */
-/*===========================================================================*/
-
-/**
- * @brief	Thread for reading command from serial (SD3).
- *
- */
-static THD_WORKING_AREA(wa_draw, 1024);
-static THD_FUNCTION(thd_draw, arg)
-{
-	chRegSetThreadName(__FUNCTION__);
-	(void)arg;
-
-	uint16_t i = 0;
-	uint16_t length = data_get_length();
-	cartesian_coord* pos = data_get_pos();
-	uint8_t* color = data_get_color();
-	uint8_t prev_color = white;
-
-//	chprintf((BaseSequentialStream *)&SDU1, "thread created \r \n");
-//	chprintf((BaseSequentialStream *)&SDU1, "length= %d \r \n", length);
-	for(i = 0; i<length && !chThdShouldTerminateX(); ++i) {
-
-		if (color[i] != prev_color) {
-			com_request_color(color[i]);
-			prev_color = color[i];
-			draw_pause_thd();
-		}
-		chSysLock();
-		if (is_paused) {
-		  chSchGoSleepS(CH_STATE_SUSPENDED);
-		}
-		chSysUnlock();
-
-//		chprintf((BaseSequentialStream *)&SD3, "COL \r \n");
-
-		draw_move(pos[i].x, pos[i].y);
-
-//		chprintf((BaseSequentialStream *)&SDU1, "loop n %d \r \n", i);
-//		chBSemWait(&move_finished_sem);
-	}
-	is_drawing = false;
-	chThdExit(0);
-}
-
-/*===========================================================================*/
 /* Module local functions.                                                	 */
 /*===========================================================================*/
 
-
+/**
+ * @brief                    Moves the robot by a certain amount of steps
+ * @param[in]   step_left    Number of steps to be traveled by left motor
+ * @param[in]   step_right   Number of steps to be traveled by right motor
+ * @param[in]   speed        Maximum speed in step/s
+ * @return                   none
+ */
 static void motor_set_step(int32_t step_left, int32_t step_right, uint16_t speed)
 {
 	// calculate absolute values for computation of sign and sleep time.
@@ -147,18 +80,14 @@ static void motor_set_step(int32_t step_left, int32_t step_right, uint16_t speed
 	float speed_right = 0;
 
 	if (step_left_abs > step_right_abs) {
-		time = 1000*step_left_abs/speed;
+		time = 1000*step_left_abs/speed; // ms
 		speed_left = speed*step_left/step_left_abs;
 		speed_right = speed * ((float)step_right/step_left_abs);
 	} else {
-		time = 1000*step_right_abs/speed;
+		time = 1000*step_right_abs/speed; // ms
 		speed_left = speed * ((float)step_left/step_right_abs);
 		speed_right = speed*step_right/step_right_abs;
 	}
-
-//	chprintf((BaseSequentialStream *)&SDU1, "left speed = %d \r \n", (int16_t)speed_left);
-//	chprintf((BaseSequentialStream *)&SDU1, "right speed = %d \r \n", (int16_t)speed_right);
-//	chprintf((BaseSequentialStream *)&SDU1, "time = %d \r \n", time);
 
 	// minus sign because of the orientation of e-puck
 	right_motor_set_speed(-speed_left);
@@ -168,20 +97,65 @@ static void motor_set_step(int32_t step_left, int32_t step_right, uint16_t speed
 	left_motor_set_speed(0);
 }
 
+/**
+ * @brief                    Offsets x position to match drawing area
+ * @param[in]   x            x coordinate
+ * @return                   x coordinate with offset
+ */
+static uint16_t offset_x_pos(uint16_t x)
+{
+	return x + (X_RESOLUTION - IM_MAX_WIDTH)/2;
+}
+
+/*===========================================================================*/
+/* Module threads.                                                   		 */
+/*===========================================================================*/
+
+/**
+ * @brief   Thread for drawing a picture from information contained in
+ *          position and color buffers (mod_data).
+ *
+ */
+static THD_WORKING_AREA(wa_draw, 1024);
+static THD_FUNCTION(thd_draw, arg)
+{
+	chRegSetThreadName(__FUNCTION__);
+	(void)arg;
+
+	uint16_t i = 0;
+	uint16_t length = data_get_length();
+
+	cartesian_coord* pos = data_get_pos();
+	uint8_t* color = data_get_color();
+	uint8_t prev_color = white;
+
+	for(i = 0; i < length && !chThdShouldTerminateX(); ++i) {
+
+		if (color[i] != prev_color) {
+			com_request_color(color[i]);
+			prev_color = color[i];
+			draw_pause_thd();
+		}
+
+		chSysLock();
+		if (is_paused) {
+		  chSchGoSleepS(CH_STATE_SUSPENDED);
+		}
+		chSysUnlock();
+
+		draw_move(offset_x_pos(pos[i].x), pos[i].y);
+	}
+
+	// reset stepper position and lift pen when drawing is complete
+	com_request_color(none);
+
+	is_drawing = false;
+	chThdExit(0);
+}
+
 /*===========================================================================*/
 /* Module exported functions.                                                */
 /*===========================================================================*/
-
-void draw_set_init_length(float y_length)
-{
-	y0_st = CM_TO_STEP*y_length;
-	len0_st = sqrt(x0_st*x0_st + y0_st*y0_st);
-//	chprintf((BaseSequentialStream *)&SDU1, "x0_st = %d \r \n",x0_st);
-//	chprintf((BaseSequentialStream *)&SDU1, "y0_st = %d \r \n",y0_st);
-//	chprintf((BaseSequentialStream *)&SDU1, "len0_st = %d \r \n",len0_st);
-//	chprintf((BaseSequentialStream *)&SDU1, "len0_CM = %f \r \n",len0_st/CM_TO_STEP);
-//	chprintf((BaseSequentialStream *)&SDU1, "x0_CM = %f \r \n",x0_st/CM_TO_STEP);
-}
 
 void draw_reset(void)
 {
@@ -195,7 +169,8 @@ void draw_reset(void)
 void draw_create_thd(void)
 {
 	if (!is_drawing) {
-		ptr_draw = chThdCreateStatic(wa_draw, sizeof(wa_draw), NORMALPRIO, thd_draw, NULL);
+		ptr_draw = chThdCreateStatic(wa_draw, sizeof(wa_draw), NORMALPRIO,
+		                             thd_draw, NULL);
 		is_drawing = true;
 	}
 }
@@ -225,6 +200,16 @@ void draw_resume_thd(void)
 	chSysUnlock();
 }
 
+bool draw_get_state(void)
+{
+	return is_drawing;
+}
+
+void draw_set_init_length(float y_length)
+{
+	y0_st = CM_TO_STEP*y_length;
+	len0_st = sqrt(x0_st*x0_st + y0_st*y0_st);
+}
 
 uint16_t draw_get_length_av_current(void)
 {
@@ -239,6 +224,7 @@ uint16_t draw_get_length_av_next(uint16_t x, uint16_t y)
 	uint16_t y_st = y*CART_TO_ST + y0_st;
 	uint16_t x_l_st = x_st + MARGIN_ST - SPOOL_DISTANCE_ST/2;
 	uint16_t x_r_st = SUPPORT_DISTANCE_ST - SPOOL_DISTANCE_ST/2 - MARGIN_ST - x_st;
+
 	// calculate next length
 	uint16_t len_l = sqrt(x_l_st*x_l_st + y_st*y_st);
 	uint16_t len_r = sqrt(x_r_st*x_r_st + y_st*y_st);
@@ -253,48 +239,21 @@ void draw_move(uint16_t x, uint16_t y)
 	uint16_t len_l_current = len0_st - right_motor_get_pos();
 	uint16_t len_r_current = len0_st - left_motor_get_pos();
 
-//	chprintf((BaseSequentialStream *)&SDU1, "len_l_current = %d \r \n",len_l_current);
-//	chprintf((BaseSequentialStream *)&SDU1, "len_r_current = %d \r \n",len_r_current);
-//	chprintf((BaseSequentialStream *)&SDU1, "spool perim cm = %f \r \n",SPOOL_PERIMETER);
-//	chprintf((BaseSequentialStream *)&SDU1, "CMTOSTEP = %f \r \n",CM_TO_STEP);
-//	chprintf((BaseSequentialStream *)&SDU1, "CARTTOST = %f \r \n",CART_TO_ST);
-//	chprintf((BaseSequentialStream *)&SDU1, "spool dist = %d \r \n",SPOOL_DISTANCE_ST);
-
-
-//	chprintf((BaseSequentialStream *)&SDU1, "l motor = %d \r \n",left_motor_get_pos());
-//	chprintf((BaseSequentialStream *)&SDU1, "r motor = %d \r \n",right_motor_get_pos());
-
 	// calculate coordinates relative to top-left attach point
 	uint16_t x_st = x*CART_TO_ST;
 	uint16_t y_st = y*CART_TO_ST + y0_st;
 	uint16_t x_l_st = x_st + MARGIN_ST - SPOOL_DISTANCE_ST/2;
 	uint16_t x_r_st = SUPPORT_DISTANCE_ST - SPOOL_DISTANCE_ST/2 - MARGIN_ST - x_st;
-//	chprintf((BaseSequentialStream *)&SDU1, "x_st = %d \r \n", x_st);
-//	chprintf((BaseSequentialStream *)&SDU1, "y_st = %d \r \n", y_st);
-//	chprintf((BaseSequentialStream *)&SDU1, "x_r_st = %d \r \n", x_r_st);
-//	chprintf((BaseSequentialStream *)&SDU1, "x_l_st = %d \r \n", x_l_st);
-//	chprintf((BaseSequentialStream *)&SDU1, "YCM %f \r \n", y*CART_TO_ST/CM_TO_STEP);
+
 	// calculate next length
 	uint16_t len_l = sqrt(x_l_st*x_l_st + y_st*y_st);
 	uint16_t len_r = sqrt(x_r_st*x_r_st + y_st*y_st);
-//	chprintf((BaseSequentialStream *)&SDU1, "len_l_CM = %f \r \n",len_l/CM_TO_STEP);
-//	chprintf((BaseSequentialStream *)&SDU1, "len_r_CM = %f \r \n",len_r/CM_TO_STEP);
-//	chprintf((BaseSequentialStream *)&SDU1, "xl_CM = %f \r \n",x_l_st/CM_TO_STEP);
-//	chprintf((BaseSequentialStream *)&SDU1, "xr_CM = %f \r \n",x_r_st/CM_TO_STEP);
-//	chprintf((BaseSequentialStream *)&SDU1, "yabs_cm = %f \r \n",y_st/CM_TO_STEP);
 
 	// move until next length
 	while (abs(len_l-len_l_current)>STEP_THRESHOLD || abs(len_r-len_r_current)>STEP_THRESHOLD) {
 		motor_set_step(len_l-len_l_current, len_r-len_r_current, MAX_SPEED);
 		len_l_current = len0_st - right_motor_get_pos();
 		len_r_current = len0_st - left_motor_get_pos();
-
-//		chprintf((BaseSequentialStream *)&SDU1, "len_l = %d \r \n", len_l);
-//		chprintf((BaseSequentialStream *)&SDU1, "len_r = %d \r \n", len_r);
-//		chprintf((BaseSequentialStream *)&SDU1, "len_l_current = %d \r \n",len_l_current);
-//		chprintf((BaseSequentialStream *)&SDU1, "len_r_current = %d \r \n",len_r_current);
-//		chprintf((BaseSequentialStream *)&SDU1, "l motor = %d \r \n",left_motor_get_pos());
-//		chprintf((BaseSequentialStream *)&SDU1, "r motor = %d \r \n",right_motor_get_pos());
 	}
 }
 
