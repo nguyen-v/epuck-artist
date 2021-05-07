9ee7fe8ad769d60e234e986eed4b3bfe8a9d2765
Vincent <vincen7.nguyen@gmail.com>
2021-05-07 14:29:15 +0200

    FIxed handling of thread suspend and semaphore for color change Added manual home point definition using IR sensors

--- src/modules/include/mod_calibration.h
+++ src/modules/include/mod_calibration.h
@@ -7,6 +7,12 @@
 #define _MOD_CALIBRATION_H_
 
 /*===========================================================================*/
+/* Exported constants                                                        */
+/*===========================================================================*/
+
+#define DEFAULT_HEIGHT_CM     100 // cm
+
+/*===========================================================================*/
 /* External declarations.                                                    */
 /*===========================================================================*/
 
@@ -39,5 +45,23 @@ bool cal_get_state(void);
  * @return                       none
  */
 void cal_signal_changed_colors(void);
+
+/**
+ * @brief                        Creates thread to set top center of canvas
+ * @return                       none
+ */
+void cal_create_home_thd(void);
+
+/**
+ * @brief                        Stops set home thread
+ * @return                       none
+ */
+void cal_stop_home_thd(void);
+
+/**
+ * @brief                        Returns current set home thread state
+ * @return                       True if currently setting home, false otherwise
+ */
+bool cal_get_home_state(void);
 
 #endif /* _MOD_CALIBRATION_H_ */
--- src/modules/mod_calibration.c
+++ src/modules/mod_calibration.c
@@ -20,6 +20,7 @@
 // e-puck2 main processor headers
 
 #include <motors.h>
+#include "sensors/proximity.h"
 
 // Module headers
 
@@ -48,24 +49,33 @@
 #define TOF_PRECISION_THRESHOLD    2       // mm
 #define TOF_STEADY_INTERVAL        3000    // ms
 
-// TOF distance thresholds in mm
+// Distance thresholds in mm for set home thread
 
-#define DIST_TOF_FORWARD_LOW       0
-#define DIST_TOF_FORWARD_HIGH      150
+#define DIST_TOF_FORWARD_LOW       100
+#define DIST_TOF_FORWARD_HIGH      250
 #define DIST_TOF_BACKWARD_LOW      200
 #define DIST_TOF_BACKWARD_MID      350
 #define DIST_TOF_BACKWARD_HIGH     450
 #define DIST_TOF_DIFF_THR          50
 
+#define IR1                        0
+#define IR2                        1
+#define IR5                        4
+#define IR7                        6
+#define IR8                        7
+#define IR_THRESHOLD               100
+
+#define SET_HOME_PERIOD            100     // ms
+
 // Initial height/length calculation
 
-#define LENGTH_DIST_SLOPE          52.7f   // found experimentally
-#define LENGTH_DIST_INTERCEPT      -14.5f  // found experimentally
+#define LENGTH_DIST_SLOPE          52.7f   // found by linear regression
+#define LENGTH_DIST_INTERCEPT      -14.5f  // found by linear regression
 #define CORRECTION_FACTOR          0.92f   // to compensate model error when
                                             // setting initial length
 
-// distance measured by TOF sensor has to be multiplied by this factor
-// because the epuck is not completely parallel to the wall
+// distance measured by TOF sensor has to be multiplied by this factor because
+// the epuck is not completely parallel to the wall (found by linear regression)
 
 #define TOF_CORRECTION_FACTOR      1.1823
 
@@ -74,8 +84,8 @@
 /*===========================================================================*/
 
 static bool is_calibrating = false;
-static bool reached_goal_distance = false;
-static bool reached_home = false;
+static bool is_setting_home = false;
+static bool is_waiting = false;
 
 /*===========================================================================*/
 /* Semaphores.                                                               */
@@ -89,6 +99,8 @@ static BSEMAPHORE_DECL(sem_changed_color, TRUE);
 
 static thread_t* ptr_calibrate;
 
+static thread_t* ptr_set_home;
+
 /*===========================================================================*/
 /* Module local functions.                                                   */
 /*===========================================================================*/
@@ -97,9 +109,12 @@ static thread_t* ptr_calibrate;
  * @brief                        P controller on speed to reach goal distance
  * @param[in]  goal_distance     Distance to reach in mm
  * @param[in]  init_distance     Initial (starting) distance in mm
+ * @param[in]  reached_goal_dist Pointer to variable indicating if e-puck
+ *                               has reached its goal distance
  * @return                       none
  */
-static int16_t get_speed_p(int16_t goal_distance, int16_t init_distance)
+static int16_t get_speed_p(int16_t goal_distance, int16_t init_distance,
+                            bool* reached_goal_dist)
 {
 	int16_t speed = 0;
 	int16_t error = 0;
@@ -110,7 +125,7 @@ static int16_t get_speed_p(int16_t goal_distance, int16_t init_distance)
 		return 0;
 
 	if (abs(error) < TOF_PRECISION_THRESHOLD) {
-		reached_goal_distance = true;
+		*reached_goal_dist = true;
 		return 0;
 	}
 
@@ -129,14 +144,16 @@ static int16_t get_speed_p(int16_t goal_distance, int16_t init_distance)
 /**
  * @brief                        P controller on motor step count to
  *                               reach initial position
+ * @param[in]  reached_home      Pointer to variable indicating if e-puck
+ *                               has returned to its original position
  * @return                       none
  */
-static int16_t move_home(void)
+static int16_t move_home(bool* reached_home)
 {
 	float speed = 0;
-	int16_t error = -(left_motor_get_pos()+right_motor_get_pos())/2;
+	int16_t error = -( left_motor_get_pos() + right_motor_get_pos() )/2;
 	if (abs(error) < TOF_PRECISION_THRESHOLD*MM_TO_STEP) {
-		reached_home = true;
+		*reached_home = true;
 	}
 	speed = KP/MM_TO_STEP * error;
 
@@ -201,17 +218,26 @@ static THD_FUNCTION(thd_calibrate, arg)
 	draw_reset();
 
 	// draw first calibration point
+	if (chThdShouldTerminateX())
+		chThdExit(0);
 	com_request_color(black);
 	chBSemWait(&sem_changed_color);
-	chThdSleepMilliseconds(100);
+
+	if (chThdShouldTerminateX())
+		chThdExit(0);
 	com_request_color(white);
 	chBSemWait(&sem_changed_color);
 	draw_move(X_DEFAULT+CALIBRATION_SQ_PX, Y_DEFAULT);
 
 	// draw second calibration point
+
+	if (chThdShouldTerminateX())
+		chThdExit(0);
 	com_request_color(black);
 	chBSemWait(&sem_changed_color);
-	chThdSleepMilliseconds(100);
+
+	if (chThdShouldTerminateX())
+		chThdExit(0);
 	com_request_color(white);
 	chBSemWait(&sem_changed_color);
 	draw_move(X_DEFAULT, Y_DEFAULT);
@@ -227,36 +253,38 @@ static THD_FUNCTION(thd_calibrate, arg)
 	palSetPad(GPIOD, GPIOD_LED_FRONT);
 
 	// wait and get goal distance value from computer
+	is_waiting = true;
 	thread_t *tp = chMsgWait();
 	msg_t goal_distance = chMsgGet(tp);
 	chMsgRelease(tp, MSG_OK);
-
+	is_waiting = false;
 	uint16_t init_distance = 0;
 
 	// turn off front led
 	palClearPad(GPIOD, GPIOD_LED_FRONT);
 
+	bool reached_goal_dist = false;
+	bool reached_home = false;
+
 	if (!chThdShouldTerminateX()) {
 		// wait until distance is steady for long enough
 		init_distance = sensors_tof_wait(TOF_DISTANCE_MIN, TOF_DISTANCE_MAX,
 		                                 TOF_PRECISION_THRESHOLD, TOF_STEADY_INTERVAL);
-		reached_goal_distance = false;
-		reached_home = false;
 	}
+
 	while (!chThdShouldTerminateX()) {
 
-		chThdSleepMilliseconds(CALIBRATION_PERIOD);
-
-		if (!reached_goal_distance) {
+		if (!reached_goal_dist) {
 			// move to goal distance and update true travel distance
-			speed = get_speed_p(TOF_CORRECTION_FACTOR*goal_distance, init_distance);
+			speed = get_speed_p(TOF_CORRECTION_FACTOR*goal_distance, init_distance,
+			                    &reached_goal_dist);
 			true_diff_length = -(left_motor_get_pos()+right_motor_get_pos())/2;
 			left_motor_set_speed(speed);
 			right_motor_set_speed(speed);
 
 		} else if (!reached_home) {
 			// move back to starting point and terminate thread
-			speed = move_home();
+			speed = move_home(&reached_home);
 			left_motor_set_speed(speed);
 			right_motor_set_speed(speed);
 
@@ -267,13 +295,63 @@ static THD_FUNCTION(thd_calibrate, arg)
 			chThdTerminate(ptr_calibrate);
 		}
 
+		chThdSleepMilliseconds(CALIBRATION_PERIOD);
+
 	}
 
-	is_calibrating = false;;
-
+	is_calibrating = false;
 	// return true length difference on exit
 	chThdExit((msg_t)true_diff_length);
 }
+
+
+
+/**
+ * @brief      Set home point (top middle of canvas)
+ * @note       Top center coordinates of canvas is (X_RESOLUTION/2, 0)
+ */
+static THD_WORKING_AREA(wa_set_home, 256);
+static THD_FUNCTION(thd_set_home, arg)
+{
+	chRegSetThreadName(__FUNCTION__);
+	(void)arg;
+
+	while (!chThdShouldTerminateX()) {
+
+		chThdSleepMilliseconds(SET_HOME_PERIOD);
+		// go up
+		if ( get_calibrated_prox(IR8) > IR_THRESHOLD
+			|| get_calibrated_prox(IR1) > IR_THRESHOLD) {
+			left_motor_set_speed(CALIBRATION_SPEED);
+			right_motor_set_speed(CALIBRATION_SPEED);
+
+		// go down
+		} else if ( get_calibrated_prox(IR5) > IR_THRESHOLD) {
+			left_motor_set_speed(-CALIBRATION_SPEED);
+			right_motor_set_speed(-CALIBRATION_SPEED);
+
+		// go left
+		} else if (get_calibrated_prox(IR7) > IR_THRESHOLD) {
+			left_motor_set_speed(-CALIBRATION_SPEED);
+			right_motor_set_speed(CALIBRATION_SPEED);
+
+		// go right
+		} else if (get_calibrated_prox(IR2) > IR_THRESHOLD) {
+			left_motor_set_speed(CALIBRATION_SPEED);
+			right_motor_set_speed(-CALIBRATION_SPEED);
+
+		} else {
+			left_motor_set_speed(0);
+			right_motor_set_speed(0);
+		}
+
+		chThdSleepMilliseconds(SET_HOME_PERIOD);
+	}
+
+	is_setting_home = false;
+	chThdExit(0);
+}
+
 
 /*===========================================================================*/
 /* Module exported functions.                                                */
@@ -290,7 +368,8 @@ void cal_set_goal_distance(void)
 void cal_create_thd(void)
 {
 	if (!is_calibrating) {
-		ptr_calibrate = chThdCreateStatic(wa_calibrate, sizeof(wa_calibrate), NORMALPRIO, thd_calibrate, NULL);
+		ptr_calibrate = chThdCreateStatic(wa_calibrate, sizeof(wa_calibrate),
+		                                  NORMALPRIO, thd_calibrate, NULL);
 		is_calibrating = true;
 	}
 }
@@ -299,10 +378,11 @@ uint16_t cal_stop_thd(void)
 {
 	if (is_calibrating) {
 
-		// send message to unblock chMsgWait()
-		(void)chMsgSend(ptr_calibrate, 0);
-
 		chThdTerminate(ptr_calibrate);
+		// send message to unblock chMsgWait()
+		if (is_waiting)
+			(void)chMsgSend(ptr_calibrate, 0);
+
 		uint16_t true_diff_length = chThdWait(ptr_calibrate);
 		is_calibrating = false;
 
@@ -319,120 +399,29 @@ bool cal_get_state(void)
 
 void cal_signal_changed_colors(void)
 {
-	chBSemSignal(&sem_changed_color);
+	if (is_calibrating)
+		chBSemSignal(&sem_changed_color);
 }
 
-//void cal_set_init_length(void)
-//{
-//	uint16_t true_diff_length = cal_stop_thd();
-//	if (true_diff_length != 0) {
-//		draw_move(X_DEFAULT, Y_DEFAULT);
-//		float diff_length_ratio = (float)true_diff_length/expected_diff_length;
-//		float corrected_length = (diff_length_ratio * LENGTH_DIST_SLOPE + LENGTH_DIST_INTERCEPT)*CORRECTION_FACTOR;
-//		draw_set_init_length(corrected_length*CORRECTION_FACTOR);
-//		chprintf((BaseSequentialStream *)&SDU1, "corrected length %f \r \n", corrected_length);
-//	}
-////	draw_set_init_length();
-//}
+void cal_create_home_thd(void)
+{
+	if (!is_setting_home) {
+		ptr_set_home = chThdCreateStatic(wa_set_home, sizeof(wa_set_home),
+		                                  NORMALPRIO, thd_set_home, NULL);
+		is_setting_home = true;
+	}
+}
 
+void cal_stop_home_thd(void)
+{
+	if (is_setting_home) {
+		chThdTerminate(ptr_set_home);
+		draw_reset();
+		is_setting_home = false;
+	}
+}
 
-//float get_initial_height_cm(void)
-//{
-//	// get initial height and length
-//	left_motor_set_pos(0);
-//	right_motor_set_pos(0);
-//	uint16_t step_left = left_motor_get_pos();
-//	uint16_t step_right = right_motor_get_pos();
-//	uint16_t step_left_fin = 0;
-//	uint16_t step_right_fin = 0;
-//	uint16_t dist = sensors_tof_kalman();
-//	uint16_t dist_fin = 0;
-//	chprintf((BaseSequentialStream *)&SDU1, "init step left %d \r \n", step_left);
-//	chprintf((BaseSequentialStream *)&SDU1, "init step right %d \r \n", step_right);
-//	chprintf((BaseSequentialStream *)&SDU1, "init dist mm %d \r \n", dist);
-//	// move down
-//	chThdSleepSeconds(2);
-////	left_motor_set_speed(-CALIBRATION_SPEED);
-////	right_motor_set_speed(-CALIBRATION_SPEED);
-////	chThdSleepMilliseconds(1000*CALIBRATION_STEPS/CALIBRATION_SPEED);
-////	left_motor_set_speed(0);
-////	right_motor_set_speed(0);
-////	chThdSleepSeconds(2);
-////
-////	// get updated height and length
-////	step_left -=  left_motor_get_pos();
-////	step_right -= right_motor_get_pos();
-////	dist_fin = sensors_tof_kalman();
-////	printf((BaseSequentialStream *)&SDU1, "init dist mm %d \r \n", dist_fin);
-////	dist -= dist_fin;
-////	chprintf((BaseSequentialStream *)&SDU1, "fin step left %d \r \n", step_left);
-////	chprintf((BaseSequentialStream *)&SDU1, "fin step right %d \r \n", step_right);
-////
-////	float dy = dist/10.; // conversion to cm
-////	float dl = (step_left + step_right)/(2*CM_TO_STEP);
-////	chprintf((BaseSequentialStream *)&SDU1, "fin dy cm %f \r \n", dy);
-////	chprintf((BaseSequentialStream *)&SDU1, "updated length %f \r \n", dl);
-////	chThdSleepSeconds(1);
-//
-//	for (uint8_t i = 0; i< 5; ++i) {
-//		left_motor_set_speed(-CALIBRATION_SPEED);
-//		right_motor_set_speed(-CALIBRATION_SPEED);
-//		chThdSleepMilliseconds(1000*CALIBRATION_STEPS/CALIBRATION_SPEED);
-//		left_motor_set_speed(0);
-//		right_motor_set_speed(0);
-//		chThdSleepMilliseconds(3000);
-//		dist_fin = sensors_tof_kalman();
-//		step_left_fin = step_left - left_motor_get_pos();
-//		step_right_fin = step_right - right_motor_get_pos();
-////		chprintf((BaseSequentialStream *)&SDU1, "dy %f \r \n", (dist-dist_fin)/10.);
-////		chprintf((BaseSequentialStream *)&SDU1, "dl %f \r \n", (step_left_fin + step_right_fin)/(2*CM_TO_STEP));
-//		chprintf((BaseSequentialStream *)&SDU1, "%f %f \r \n", (dist-dist_fin)/10.,(step_left_fin + step_right_fin)/(2*CM_TO_STEP));
-//
-//	}
-//
-//	// move back up
-//	left_motor_set_speed(CALIBRATION_SPEED);
-//	right_motor_set_speed(CALIBRATION_SPEED);
-//	chThdSleepMilliseconds(1000*5*CALIBRATION_STEPS/CALIBRATION_SPEED);
-//	left_motor_set_speed(0);
-//	right_motor_set_speed(0);
-//
-//	// calculate inital height
-////	float dx_sq = dl*dl - dy*dy;
-////	float x = (SUPPORT_DISTANCE-SPOOL_DISTANCE)/2.;
-////	chprintf((BaseSequentialStream *)&SDU1, "X %f \r \n", x);
-////	chprintf((BaseSequentialStream *)&SDU1, "dxsq %f \r \n", dx_sq);
-////	return -(dl*sqrt(-dx_sq*(4*x*x-dx_sq))+dx_sq*dy)/(2.*dx_sq);
-////	return -(dl*sqrt(-dx_sq*(4*x*x-dx_sq))-dx_sq*dy)/(2.*dx_sq);
-//	return 1;
-//}
-
-//void calibration_height(void)
-//{
-//
-//}
-
-
-// get current length and distance
-//		dist = sensors_tof_kalman();
-//		step = (left_motor_get_pos()+right_motor_get_pos())/2;
-//
-//		if(dist>DIST_TOF_FORWARD_LOW && dist < DIST_TOF_FORWARD_HIGH
-//				&& abs(dist-prev_dist) < DIST_TOF_DIFF_THR && step < step_init) {
-//			left_motor_set_speed(CALIBRATION_SPEED*(DIST_TOF_FORWARD_HIGH-dist)/DIST_TOF_FORWARD_HIGH);
-//			right_motor_set_speed(CALIBRATION_SPEED*(DIST_TOF_FORWARD_HIGH-dist)/DIST_TOF_FORWARD_HIGH);
-//		} else if (dist>DIST_TOF_BACKWARD_LOW && dist < DIST_TOF_BACKWARD_MID && abs(dist-prev_dist) < DIST_TOF_DIFF_THR) {
-//			left_motor_set_speed(-CALIBRATION_SPEED*(dist-DIST_TOF_BACKWARD_LOW)/(DIST_TOF_BACKWARD_MID-DIST_TOF_BACKWARD_LOW));
-//			right_motor_set_speed(-CALIBRATION_SPEED*(dist-DIST_TOF_BACKWARD_LOW)/(DIST_TOF_BACKWARD_MID-DIST_TOF_BACKWARD_LOW));
-//		} else if (dist>DIST_TOF_BACKWARD_MID && dist < DIST_TOF_BACKWARD_HIGH && abs(dist-prev_dist) < DIST_TOF_DIFF_THR) {
-//			left_motor_set_speed(-CALIBRATION_SPEED*(DIST_TOF_BACKWARD_HIGH-dist)/(DIST_TOF_BACKWARD_HIGH-DIST_TOF_BACKWARD_MID));
-//			right_motor_set_speed(-CALIBRATION_SPEED*(DIST_TOF_BACKWARD_HIGH-dist)/(DIST_TOF_BACKWARD_HIGH-DIST_TOF_BACKWARD_MID));
-//		} else {
-//			left_motor_set_speed(0);
-//			right_motor_set_speed(0);
-//		}
-//
-//		//
-//		true_diff_length = step_init - (left_motor_get_pos()+right_motor_get_pos())/2;
-//		chprintf((BaseSequentialStream *)&SDU1, "true length %d \r \n", true_diff_length);
-//		chprintf((BaseSequentialStream *)&SDU1, "expected length %d \r \n", expected_diff_length);
+bool cal_get_home_state(void)
+{
+	return is_setting_home;
+}
